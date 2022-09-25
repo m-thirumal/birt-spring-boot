4f6df32bf6e09431342eee259ca81d61a5cad185
Thirumal <m.thirumal@hotmail.com>
2022-09-25 20:37:23 +0530

    first commit

---
+++ src/main/java/com/thirumal/BirtReportApplication.java
@@ -0,0 +1,13 @@
+package com.thirumal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirtReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BirtReportApplication.class, args);
+	}
+
+}
